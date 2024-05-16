96f92e9ff9aeb466a35d6fa5e3c9afe56ba03bb2
stanimirdim92 <stanimirdim92@gmail.com>
2024-05-16 15:51:10 +0300

    cleanup

---
+++ The Rust Programming Language/Chapter 3/src/main.rs
@@ -0,0 +1,30 @@
+use std::fs::{File, Metadata};
+use std::io::{BufWriter, Error, Write};
+
+#[tokio::main]
+async fn main() {
+    another_function_two(5, 5, '\u{2665}');
+    write_to_file(256).await.expect("TODO: panic message");
+}
+
+fn another_function_two(x: i32, y: i32, char: char) {
+    println!("{} {}", x * y, char);
+}
+
+async fn write_to_file(number: usize) -> Result<(), Error> {
+    let file: File = File::create("file.txt").expect("unable to create file");
+    let mut buf = BufWriter::new(&file);
+
+    for x in 0..number {
+        buf.write((x.to_string()+"\r\n").as_bytes()).expect("TODO: panic message");
+    }
+
+    file.sync_all()?;
+    buf.flush()?;
+
+    file.metadata()
+        .iter()
+        .for_each(|v: &Metadata| println!("{:?}", v));
+
+    Ok(())
+}
