ca5d20c28543a4133284122e4c6da368e49849af
stanimirdim92 <stanimirdim92@gmail.com>
2024-05-25 22:39:30 +0300

    cleanup

---
+++ The Rust Programming Language/Chapter 6/enums/target/debug/deps/enums-8127a92a82d98be2.d
@@ -0,0 +1,5 @@
+/var/www/html/rust/The Rust Programming Language/Chapter 6/enums/target/debug/deps/enums-8127a92a82d98be2: src/main.rs
+
+/var/www/html/rust/The Rust Programming Language/Chapter 6/enums/target/debug/deps/enums-8127a92a82d98be2.d: src/main.rs
+
+src/main.rs:
