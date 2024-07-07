41f5a47489b35404d3e5281234e48305b63554e8
stanimirdim92 <stanimirdim92@gmail.com>
2024-07-07 22:53:16 +0300

    traits

--- The Rust Programming Language/Chapter 10/traits/src/main.rs
+++ The Rust Programming Language/Chapter 10/traits/src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 trait Shape {
     fn shape(&self) -> String;
     fn size(&self, width: u32) -> u32 {
@@ -20,19 +22,54 @@ impl Shape for Square {
     }
 
     fn size(&self, width: u32) -> u32 {
-        self.width * 3
+        self.width * width
+    }
+}
+
+#[derive(Copy, Clone)]
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
+
+impl<T: Display, U: Display> Display for Point<T, U> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({}, {})", self.x, self.y)
+    }
+}
+
+impl<T: Display + Clone, U: Display + Clone> Point<T, U> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+
+    fn y(&self) -> &U {
+        &self.y
+    }
+
+    fn origin(x: T, y: U) -> Self {
+        Self { x, y }
+    }
+
+    fn show(&self) -> String {
+        format!("({}, {})", self.x, self.y)
     }
 }
 
 
 fn main() {
     let square = Square { width: 5 };
-
-
     println!("{}", square.shape());
     println!("{}", square.size());
-    let size_from_shape_trait = Shape::size(&square, square.width);
-    println!("Size from Shape trait: {}", size_from_shape_trait);
+    println!("Size from Shape trait: {}", Shape::size(&square, 6));
+
+
+    let point = Point::origin(50, 10.5);
+
+    println!("{}", point);
+    println!("{}", point.x());
+    println!("{}", point.y());
+    println!("{}", point.show());
 }
 
 
